44c24f2822375df20702918bce9ab760296674f4
KevinBacas <bacas.kevin@hotmail.fr>
2015-11-15 15:52:36 +0100

    Deleted Middlewares + Changed urls to / and /blog/

--- FuzzeyPot/FuzzeyPot/urls.py
+++ FuzzeyPot/FuzzeyPot/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('Blog.urls', namespace='blog')),
     url(r'^blog/', include('Blog.urls', namespace='blog')),
 ]
